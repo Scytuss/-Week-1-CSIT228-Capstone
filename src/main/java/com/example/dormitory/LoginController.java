0a3d4061e8bdf07d850c1ab19ef3077bbd3e1774
NateRateLoL <nathanroy.gonzales@cit.edu>
2026-05-12 18:21:06 +0800

    Updated for users

--- src/main/java/com/example/dormitory/LoginController.java
+++ src/main/java/com/example/dormitory/LoginController.java
@@ -12,14 +12,15 @@ public class LoginController {
     private void handleLogin(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(
-                    getClass().getResource("main-view.fxml")
+                    getClass().getResource("user-main-view.fxml")
             );
 
             Scene scene = new Scene(loader.load());
 
             // Get current window
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
+            stage.setMinWidth(1000);
+            stage.setMinHeight(520);
             stage.setScene(scene);
             stage.setTitle("Dashboard");
 
