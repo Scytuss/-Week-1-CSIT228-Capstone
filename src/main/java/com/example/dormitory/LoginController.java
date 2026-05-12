f3cfe444a89d4b14c944a93babc5a8d6bbb8fff6
Scytuss <raulmondido13@gmail.com>
2026-05-12 15:16:20 +0800

    Update LoginController.java

--- src/main/java/com/example/dormitory/LoginController.java
+++ src/main/java/com/example/dormitory/LoginController.java
@@ -1,11 +1,11 @@
-package com.example.dormitory;
+package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Node;
-import javafx.event.ActionEvent;
 
 public class LoginController {
     @FXML
